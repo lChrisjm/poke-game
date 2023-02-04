d9b2a1f2611ef58d6894c7301e5a3d9adaa4db94
lChrisjm <43703371+lChrisjm@users.noreply.github.com>
2023-02-04 00:59:05 -0400

    changing the header text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="flex py-3 bg-poke-red px-2 text-white items-center justify-between">
-      <span className="font-start">Poke-Trivia</span>
+      <span className="font-start">Poke-Quest</span>
       <a href="https://github.com/lChrisjm/poke-game" target="_blank">
         <img className="invert" src="/img/icon-gb.svg" alt="Ícono Github" />
       </a>
